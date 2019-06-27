541d26c0905533c6bf9c316c6b5e4153e0359ea0
sticklercarla <carlastickler@gmail.com>
2019-06-26 21:10:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(renderBooks);
 }
 
 function renderBooks(json) {
@@ -9,6 +11,7 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+  return 6665567657567
 }
 
 document.addEventListener('DOMContentLoaded', function() {
